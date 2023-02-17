98098ace86d2966cc3634b27e178015c0308519d
anawat moonmanee <Nhuengzii>
2023-02-17 19:51:35 +0700

    show realtime diagnostig

--- lua/plugins-config/lsp.lua
+++ lua/plugins-config/lsp.lua
@@ -14,6 +14,14 @@ lsp.ensure_installed({
 })
 
 lsp.setup()
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  update_in_insert = true,
+  underline = true,
+  severity_sort = false,
+  float = true,
+})
 
 vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
 
